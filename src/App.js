58531637bc8d30ebad0c3ddadc070eee3c2c56b9
Fernando Lima <fernandovlima.s@gmail.com>
2019-05-09 21:17:46 -0300

    implementando router entre app e busca

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
-
+import { Route } from "react-router-dom";
+import { Link } from "react-router-dom";
 //componentes
 import Estantes from "./components/Estantes";
+import Busca from "./components/Busca";
 
 class BooksApp extends React.Component {
   state = {
@@ -35,48 +37,31 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button
-                className="close-search"
-                onClick={() => this.setState({ showSearchPage: false })}
-              >
-                Close
-              </button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid" />
-            </div>
+        <Route
+          exact
+          path="/busca"
+          render={history => <Busca listaLivros={this.state.listaLivros} />}
+        />
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
           </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <Estantes
-              listaLivros={this.state.listaLivros}
-              atualizaEstante={this.atualizaEstante}
-            />
 
-            <div className="open-search">
-              <button onClick={() => this.setState({ showSearchPage: true })}>
-                Add a book
-              </button>
-            </div>
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <Estantes
+                listaLivros={this.state.listaLivros}
+                atualizaEstante={this.atualizaEstante}
+              />
+            )}
+          />
+
+          <div className="open-search">
+            <Link to="/busca">search a book</Link>
           </div>
-        )}
+        </div>
       </div>
     );
   }
